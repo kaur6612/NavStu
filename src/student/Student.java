b0668b1ce640fdf7381f487b106b112e2a2f34d0
kaur6612 <kaur6612@sheridancollege.ca>
2019-05-16 16:48:43 -0400

    MyStudent

---
+++ src/student/Student.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author waheguru
+ */
+public class Student {
+    private String name; //Fields
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1=new Student();
+        s1.setName("deep");
+        System.out.println(s1.getName());
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
